e0ca1684bf6e6e84b7421569f2858c93d1757edf
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2021-12-21 21:26:34 +0100

    cleaning

--- init_images_and_text.js
+++ init_images_and_text.js
@@ -14,29 +14,13 @@ document.writeln(' <h1>2D Arrays</h1>')
 for (let key of Object.keys(jsonData1)) {
     document.write(`<img src='imgs/${key}' alt= '${jsonData1[key]}' onclick='myFunction(this);' class='image'> `);
 }
-var request2 = new XMLHttpRequest();
-request2.open("GET", "imgs/Mobjects_Text.json", false);
-request2.send(null);
-var jsonData2 = JSON.parse(request2.responseText);
-document.writeln(' <h1>Text</h1>')
-for (let key of Object.keys(jsonData2)) {
-    document.write(`<img src='imgs/${key}' alt= '${jsonData2[key]}' onclick='myFunction(this);' class='image'> `);
-}
 
-var request3 = new XMLHttpRequest();
-request3.open("GET", "imgs/Camera_and_Style.json", false);
-request3.send(null);
-var jsonData3 = JSON.parse(request3.responseText);
-document.writeln(' <h1>Camera and Style</h1>')
-for (let key of Object.keys(jsonData3)) {
-    document.write(`<img src='imgs/${key}' alt= '${jsonData3[key]}' onclick='myFunction(this);' class='image'> `);
-}
 
 var request4 = new XMLHttpRequest();
-request4.open("GET", "imgs/Plots.json", false);
+request4.open("GET", "imgs/structuing_elements.json", false);
 request4.send(null);
 var jsonData4 = JSON.parse(request4.responseText);
-document.writeln(' <h1>Plots</h1>')
+document.writeln(' <h1>3D Structuing Elements</h1>')
 for (let key of Object.keys(jsonData4)) {
     document.write(`<img src='imgs/${key}' alt= '${jsonData4[key]}' onclick='myFunction(this);' class='imagebig'> `);
 }
