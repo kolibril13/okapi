6789dc65f2555970eb0ddc4b9754c80f36ec3232
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-10-24 15:13:03 +0200

    add script

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,147 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Canvas } from '@react-three/fiber'
+import Polyhedron from './Polyhedron'
+import * as THREE from 'three'
+import { Stats, OrbitControls } from '@react-three/drei'
+import { useControls } from 'leva'
+import { useRef } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+function Lights() {
+  const ambientRef = useRef()
+  const directionalRef = useRef()
+  const pointRef = useRef()
+  const spotRef = useRef()
+
+  useControls('Ambient Light', {
+    visible: {
+      value: false,
+      onChange: (v) => {
+        ambientRef.current.visible = v
+      },
+    },
+    color: {
+      value: 'white',
+      onChange: (v) => {
+        ambientRef.current.color = new THREE.Color(v)
+      },
+    },
+  })
+
+  useControls('Directional Light', {
+    visible: {
+      value: true,
+      onChange: (v) => {
+        directionalRef.current.visible = v
+      },
+    },
+    position: {
+      x: 1,
+      y: 1,
+      z: 1,
+      onChange: (v) => {
+        directionalRef.current.position.copy(v)
+      },
+    },
+    color: {
+      value: 'white',
+      onChange: (v) => {
+        directionalRef.current.color = new THREE.Color(v)
+      },
+    },
+  })
+
+  useControls('Point Light', {
+    visible: {
+      value: false,
+      onChange: (v) => {
+        pointRef.current.visible = v
+      },
+    },
+    position: {
+      x: 2,
+      y: 0,
+      z: 0,
+      onChange: (v) => {
+        pointRef.current.position.copy(v)
+      },
+    },
+    color: {
+      value: 'white',
+      onChange: (v) => {
+        pointRef.current.color = new THREE.Color(v)
+      },
+    },
+  })
+
+  useControls('Spot Light', {
+    visible: {
+      value: false,
+      onChange: (v) => {
+        spotRef.current.visible = v
+      },
+    },
+    position: {
+      x: 3,
+      y: 2.5,
+      z: 1,
+      onChange: (v) => {
+        spotRef.current.position.copy(v)
+      },
+    },
+    color: {
+      value: 'white',
+      onChange: (v) => {
+        spotRef.current.color = new THREE.Color(v)
+      },
+    },
+  })
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ambientLight ref={ambientRef} />
+      <directionalLight ref={directionalRef} />
+      <pointLight ref={pointRef} />
+      <spotLight ref={spotRef} />
     </>
   )
 }
 
-export default App
+export default function App() {
+  return (
+    <Canvas camera={{ position: [4, 4, 1.5] }}>
+      <Lights />
+      <Polyhedron
+        name="meshBasicMaterial"
+        position={[-3, 1, 0]}
+        material={
+          new THREE.MeshBasicMaterial({ color: 'yellow', flatShading: true })
+        }
+      />
+      <Polyhedron
+        name="meshNormalMaterial"
+        position={[-1, 1, 0]}
+        material={new THREE.MeshNormalMaterial({ flatShading: true })}
+      />
+      <Polyhedron
+        name="meshPhongMaterial"
+        position={[1, 1, 0]}
+        material={
+          new THREE.MeshPhongMaterial({ color: 'lime', flatShading: true })
+        }
+      />
+      <Polyhedron
+        name="meshStandardMaterial"
+        position={[3, 1, 0]}
+        material={
+          new THREE.MeshStandardMaterial({
+            color: 0xff0033,
+            flatShading: true,
+          })
+        }
+      />
+      <OrbitControls target={[2, 2, 0]} />
+      <axesHelper args={[5]} />
+      <gridHelper />
+      <Stats />
+    </Canvas>
+  )
+}
---
+++ src/Polyhedron.tsx
@@ -0,0 +1,17 @@
+import { useRef } from 'react'
+import { useFrame } from '@react-three/fiber'
+
+export default function Polyhedron(props) {
+  const ref = useRef()
+
+  useFrame((_, delta) => {
+    ref.current.rotation.x += 0.2 * delta
+    ref.current.rotation.y += 0.05 * delta
+  })
+
+  return (
+    <mesh {...props} ref={ref}>
+      <icosahedronGeometry args={[1, 1]} />
+    </mesh>
+  )
+}
