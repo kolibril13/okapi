90add37eb8106965895876e1d06b807a380e976a
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-10-24 16:30:28 +0200

    add directional light

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
-import { useRef } from "react";
-import { Canvas } from "@react-three/fiber";
+
+
+import { useRef, useState } from "react";
+import { Canvas, useThree, useFrame } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 
 function Polyhedron(props) {
   const ref = useRef();
-
   return (
     <mesh {...props} ref={ref}>
-      <icosahedronGeometry args={[3, 1]} />{" "}
-      <meshStandardMaterial color="green" />{" "}
+      <icosahedronGeometry args={[3, 1]} />
+      <meshStandardMaterial color="green" />
     </mesh>
   );
+}
+
+function DirectionalLight() {
+  const { camera } = useThree();
+  const [lightPosition, setLightPosition] = useState([0,0,0]);
+
+  useFrame(() => {
+    // Update the light position to match the camera position
+    setLightPosition([camera.position.x, camera.position.y, camera.position.z]);
+  });
+
+  return <directionalLight position={lightPosition} intensity={10} />;
 }
 
 export default function App() {
   return (
     <div style={{ height: "50vh", width: "100vw" }}>
       <Canvas>
-        <gridHelper args={[10, 10]} />
-        <directionalLight position={[0, 5, 5]} intensity={10} />{" "}
+        <DirectionalLight />
         <Polyhedron position={[0, 0, 0]} />
+        <gridHelper args={[20, 20]} />
         <OrbitControls />
       </Canvas>
     </div>
