455a317cd48b07d0470eed63e19744f0b4042572
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-10-24 16:28:34 +0200

    minimal

--- src/App.tsx
+++ src/App.tsx
@@ -1,147 +1,27 @@
-import { Canvas } from '@react-three/fiber'
-import Polyhedron from './Polyhedron'
-import * as THREE from 'three'
-import { Stats, OrbitControls } from '@react-three/drei'
-import { useControls } from 'leva'
-import { useRef } from 'react'
+import { useRef } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
 
-function Lights() {
-  const ambientRef = useRef()
-  const directionalRef = useRef()
-  const pointRef = useRef()
-  const spotRef = useRef()
-
-  useControls('Ambient Light', {
-    visible: {
-      value: false,
-      onChange: (v) => {
-        ambientRef.current.visible = v
-      },
-    },
-    color: {
-      value: 'white',
-      onChange: (v) => {
-        ambientRef.current.color = new THREE.Color(v)
-      },
-    },
-  })
-
-  useControls('Directional Light', {
-    visible: {
-      value: true,
-      onChange: (v) => {
-        directionalRef.current.visible = v
-      },
-    },
-    position: {
-      x: 1,
-      y: 1,
-      z: 1,
-      onChange: (v) => {
-        directionalRef.current.position.copy(v)
-      },
-    },
-    color: {
-      value: 'white',
-      onChange: (v) => {
-        directionalRef.current.color = new THREE.Color(v)
-      },
-    },
-  })
-
-  useControls('Point Light', {
-    visible: {
-      value: false,
-      onChange: (v) => {
-        pointRef.current.visible = v
-      },
-    },
-    position: {
-      x: 2,
-      y: 0,
-      z: 0,
-      onChange: (v) => {
-        pointRef.current.position.copy(v)
-      },
-    },
-    color: {
-      value: 'white',
-      onChange: (v) => {
-        pointRef.current.color = new THREE.Color(v)
-      },
-    },
-  })
-
-  useControls('Spot Light', {
-    visible: {
-      value: false,
-      onChange: (v) => {
-        spotRef.current.visible = v
-      },
-    },
-    position: {
-      x: 3,
-      y: 2.5,
-      z: 1,
-      onChange: (v) => {
-        spotRef.current.position.copy(v)
-      },
-    },
-    color: {
-      value: 'white',
-      onChange: (v) => {
-        spotRef.current.color = new THREE.Color(v)
-      },
-    },
-  })
+function Polyhedron(props) {
+  const ref = useRef();
 
   return (
-    <>
-      <ambientLight ref={ambientRef} />
-      <directionalLight ref={directionalRef} />
-      <pointLight ref={pointRef} />
-      <spotLight ref={spotRef} />
-    </>
-  )
+    <mesh {...props} ref={ref}>
+      <icosahedronGeometry args={[3, 1]} />{" "}
+      <meshStandardMaterial color="green" />{" "}
+    </mesh>
+  );
 }
 
 export default function App() {
   return (
-    <Canvas camera={{ position: [4, 4, 1.5] }}>
-      <Lights />
-      <Polyhedron
-        name="meshBasicMaterial"
-        position={[-3, 1, 0]}
-        material={
-          new THREE.MeshBasicMaterial({ color: 'yellow', flatShading: true })
-        }
-      />
-      <Polyhedron
-        name="meshNormalMaterial"
-        position={[-1, 1, 0]}
-        material={new THREE.MeshNormalMaterial({ flatShading: true })}
-      />
-      <Polyhedron
-        name="meshPhongMaterial"
-        position={[1, 1, 0]}
-        material={
-          new THREE.MeshPhongMaterial({ color: 'lime', flatShading: true })
-        }
-      />
-      <Polyhedron
-        name="meshStandardMaterial"
-        position={[3, 1, 0]}
-        material={
-          new THREE.MeshStandardMaterial({
-            color: 0xff0033,
-            flatShading: true,
-          })
-        }
-      />
-      <OrbitControls target={[2, 2, 0]} />
-      <axesHelper args={[5]} />
-      <gridHelper />
-      <Stats />
-    </Canvas>
-  )
+    <div style={{ height: "50vh", width: "100vw" }}>
+      <Canvas>
+        <gridHelper args={[10, 10]} />
+        <directionalLight position={[0, 5, 5]} intensity={10} />{" "}
+        <Polyhedron position={[0, 0, 0]} />
+        <OrbitControls />
+      </Canvas>
+    </div>
+  );
 }
